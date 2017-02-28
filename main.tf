e3d4a21d9b25aca2e89e0685d47c1a7f4e70fbcc
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2017-02-28 23:52:52 +0000

    working netscaler servicgroup interpolated group member

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
-main_override.tf
+
+# this is the top level tf file.  
+#
+# switch your environments around at this level
+
+
+terraform {
+  required_version = "~> 0.8.5"
+}
+
+module "base" {
+  source = "./base"
+}
+
+
+# you should really only edit below these lines
+# you should really only edit below these lines
+# you should really only edit below these lines
+
+module "environment" {
+  source = "./nx1_dev_rs"
+}
