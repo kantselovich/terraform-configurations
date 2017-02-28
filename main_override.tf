4a8f742756e16b6c7cf601b594a0b13d65d2133b
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2017-02-28 20:52:23 +0000

    updates

--- main_override.tf
+++ main_override.tf
@@ -1,28 +1,32 @@
 
 # this is the top level tf file.  anything set here overrides all other loaded tf files
-# if you want to figure out overrides in tf go here -> https://www.terraform.io/docs/configuration/override.html
-#
-# primary user interface starts here
+# if you want more info on overrides in go here -> https://www.terraform.io/docs/configuration/override.html
+
+
+
+## primary user interface starts here
+## primary user interface starts here
+## primary user interface starts here
 
 terraform {
   required_version = "~> 0.8.5"
 }
 
+# a failure here means that powerdns isn't setup for whatever domain
+# you're attempting to terraform
 variable "domain" {
-  # a failure here means that powerdns isn't setup for whatever domain
-  # you're attempting to terraform
   default = "nx1.dev.wh.reachlocal.com"
 }
 
+# could be any one of usa, aus, can, gbr, eur, jpn
+# use what you need but not more
 variable "platform" {
-  # could be any one of usa, aus, can, gbr, eur, jpn
-  # use what you need but not more
   default = ["usa"] 
 }
 
-variable "environment" {
-  # the purpose here is to load *only* those modules necessary for your specific terraform 
-  # options are "dev_nx1_rs", "dev_nx1_search", "dev_nx1_display"
+# the purpose here is to load *only* those modules necessary for your specific terraform 
+# options are "dev_nx1_rs", "dev_nx1_search", "dev_nx1_display"
+module "environment" {
   default = "dev_nx1_rs"
 }
 
@@ -30,19 +34,11 @@ variable "environment" {
 
 
 
-
 ## DO NOT EDIT BELOW THESE LINES
 ## DO NOT EDIT BELOW THESE LINES
 ## DO NOT EDIT BELOW THESE LINES
 
-module "common" {
+module "base" {
   source = "./common"
 }
 
-module "dev_nx1_rs" {
-  source = "./dev_nx1_rs"
-}
-
-module "dev_nx1_search" {
-  source = "./dev_nx1_search"
-}
