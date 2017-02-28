deb2150473ddc11943a55d34df0cadd000f73e15
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2017-02-28 20:10:57 +0000

    slight refactoring

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 
+# this is the top level tf file.  anything set here overrides all other loaded tf files
+# if you want to figure out overrides in tf go here -> https://www.terraform.io/docs/configuration/override.html
+#
+# primary user interface starts here
 
 terraform {
   required_version = "~> 0.8.5"
 }
 
 variable "domain" {
-  # this consequently is your environment (aka dev, qa, stg, prod)
   # a failure here means that powerdns isn't setup for whatever domain
   # you're attempting to terraform
   default = "nx1.dev.wh.reachlocal.com"
@@ -13,6 +16,33 @@ variable "domain" {
 
 variable "platform" {
   # could be any one of usa, aus, can, gbr, eur, jpn
+  # use what you need but not more
   default = ["usa"] 
 }
 
+variable "environment" {
+  # the purpose here is to load *only* those modules necessary for your specific terraform 
+  # options are "dev_nx1_rs", "dev_nx1_search", "dev_nx1_display"
+  default = "dev_nx1_rs"
+}
+
+
+
+
+
+
+## DO NOT EDIT BELOW THESE LINES
+## DO NOT EDIT BELOW THESE LINES
+## DO NOT EDIT BELOW THESE LINES
+
+module "common" {
+  source = "./common"
+}
+
+module "dev_nx1_rs" {
+  source = "./dev_nx1_rs"
+}
+
+module "dev_nx1_search" {
+  source = "./dev_nx1_search"
+}
--- powerdns_variables.tf
+++
@@ -1,9 +0,0 @@
-
-variable "pdns_api_key" {
-  default = "otto"
-}
-
-variable "pdns_server_url" {
-  default = "http://rlpc.wh.reachlocal.com:8080"
-}
-
